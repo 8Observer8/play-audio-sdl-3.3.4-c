8219fe5f4285021de744897201f04c98d3ce9cb7
Ivan Enzhaev <8observer8@gmail.com>
2025-12-12 01:52:16 +0400

    Initial commit

---
+++ README.md
@@ -0,0 +1,82 @@
+# Play Audio SDL3 (C)
+
+**Demo in the browser:** https://play-audio-sdl-3-3-4-c.netlify.app
+
+**EXE for Windows 10, 64-bit:** [play-audio-sdl3-3.3.4-c-exe.zip](https://www.dropbox.com/scl/fi/fdgpxseyi878pwm4c61l2/play-audio-sdl-3.3.4-c-exe.zip?rlkey=nvcmuspr21t1zurd1yb9l00q1&st=l0c5pxyt&raw=1)
+- Zip size: 1.58 MB
+- Unzipped: 3.43 MB
+
+---
+
+## File Sizes
+
+**WASM ( built with Emscripten 4.0.15):**
+
+- `index.html` — 1.32 KB
+- `app.js` — 183 KB
+- `app.wasm` — 1.53 MB
+- **Total:** 1.71 MB
+
+**EXE (built with MinGW GCC 11.2):**
+
+- EXE — 53.5 KB
+- `SDL3.dll` (v3.3.4) — 2.65 MB
+- `SDL3_mixer.dll`(v3.1.0) — 304 KB
+- Assets ([picked-coin-echo-2.wav](https://opengameart.org/content/picked-coin-echo-2)) — 444 KB
+- **Total:** 3.43 MB
+
+---
+
+## Tools to Build the EXE
+
+- [MinGW GCC 11.2](https://github.com/brechtsanders/winlibs_mingw/releases/download/11.2.0-14.0.0-9.0.0-ucrt-r7/winlibs-x86_64-posix-seh-gcc-11.2.0-mingw-w64ucrt-9.0.0-r7.zip)
+- [SDL3-3.3.4-win32-x64.zip](https://www.dropbox.com/scl/fi/6zojft1cdj2kzpljj2j5c/SDL3-3.3.4-win32-x64.zip?rlkey=lxvf4lzsfiv846ajpnic49s5j&st=3ukhpza9&raw=1)
+- [SDL3-devel-3.3.4-mingw.zip](https://www.dropbox.com/scl/fi/njib4pzrkuplxkomqihhg/SDL3-devel-3.3.4-mingw.zip?rlkey=69bhrsjx2w4j4ca1rc5arfbq5&st=x041deaf&raw=1)
+- [SDL3_mixer-3.1.0-win32-x64.zip](https://www.dropbox.com/scl/fi/yeiqe8wg41uw9u52qdmh7/SDL3_mixer-3.1.0-win32-x64.zip?rlkey=4teql5s5da9pn4eaz630idk21&st=hm6whpnj&raw=1)
+- [SDL3_mixer-devel-3.1.0-mingw.zip](https://www.dropbox.com/scl/fi/kpnoygzarw3o0jvfs06vs/SDL3_mixer-devel-3.1.0-mingw.zip?rlkey=y4ssud1webh3fvu103hbsgecp&st=htx3l1rh&raw=1)
+
+---
+
+## Tools to Build WebAssembly
+
+- [Emscripten 4.0.15](https://emscripten.org/docs/getting_started/downloads.html)
+- [SDL3-devel-3.3.4-wasm.zip](https://www.dropbox.com/scl/fi/ogokwxxc64f0lgf01q9oe/SDL3-devel-3.3.4-wasm.zip?rlkey=p2ggy9ynptdlbroi9ixaj1d2c&st=k2fyu1tk&raw=1)
+- [SDL3_mixer-devel-3.1.0-wasm.zip](https://www.dropbox.com/scl/fi/0e5cqe5g62idz067ihi8m/SDL3_mixer-devel-3.1.0-wasm.zip?rlkey=099fnwa4mcb5qexntzmwsfai7&st=z1lj7bjo&raw=1)
+
+---
+
+## How to Build EXE (Windows)
+
+- Add [MinGW GCC 11.2](https://github.com/brechtsanders/winlibs_mingw/releases/download/11.2.0-14.0.0-9.0.0-ucrt-r7/winlibs-x86_64-posix-seh-gcc-11.2.0-mingw-w64ucrt-9.0.0-r7.zip) `bin` folder to your `PATH`.
+- Add [SDL3-3.3.4-win32-x64.zip](https://www.dropbox.com/scl/fi/6zojft1cdj2kzpljj2j5c/SDL3-3.3.4-win32-x64.zip?rlkey=lxvf4lzsfiv846ajpnic49s5j&st=3ukhpza9&raw=1) folder to your `PATH`.
+- Add [SDL3_mixer-3.1.0-win32-x64.zip](https://www.dropbox.com/scl/fi/yeiqe8wg41uw9u52qdmh7/SDL3_mixer-3.1.0-win32-x64.zip?rlkey=4teql5s5da9pn4eaz630idk21&st=hm6whpnj&raw=1) folder to your `PATH`.
+- Configure paths in `config-web.bat` for your SDL3 libraries.
+- From the project root, run:
+
+```bat
+config-exe
+build-exe
+run-exe
+```
+
+## How to Build WebAssembly
+
+1. Set up [Emscripten 4.0.15](https://emscripten.org/docs/getting_started/downloads.html) and Node.js
+2. Install http-server globally:
+
+```bat
+npm i http-server -g
+```
+
+3. Configure paths in `config-web.bat` for your SDL3 libraries.
+4. From the project root, run:
+
+```bat
+config-web
+build-web
+http-server -c-1
+```
+
+5. Open your browser and go to http://localhost:8080 to run the app locally.
+
+> Note: You can also run the WASM app on Android via Wi-Fi using http-server. After starting the server, it will show local URL addresses — just type one into your mobile browser.
---
+++ src/main.c
@@ -0,0 +1,171 @@
+#define SDL_MAIN_USE_CALLBACKS 1
+#include <SDL3/SDL.h>
+#include <SDL3/SDL_main.h>
+#include <SDL3_mixer/SDL_mixer.h>
+
+static SDL_Window *window = NULL;
+static SDL_Renderer *renderer = NULL;
+
+static MIX_Mixer *mixer = NULL;
+static MIX_Audio *audio = NULL;
+
+SDL_AppResult SDL_AppInit(void **appstate, int argc, char **argv)
+{
+    // --- Disable unwanted decoders ---
+    SDL_SetHint("SDL_MIXER_DISABLE_DRFLAC", "1");
+    SDL_SetHint("SDL_MIXER_DISABLE_DRMP3", "1");
+
+    // --- Init SDL ---
+    if (!SDL_Init(SDL_INIT_VIDEO))
+    { // you used SDL_INIT_VIDEO in python
+        SDL_Log("Couldn't initialize SDL: %s", SDL_GetError());
+        return SDL_APP_FAILURE;
+    }
+
+    // --- Create window and renderer ---
+    const char *title = "Play a sound by click using SDL3 C";
+    if (!SDL_CreateWindowAndRenderer(title, 380, 380, 0, &window, &renderer))
+    {
+        SDL_Log("Couldn't create window/renderer: %s", SDL_GetError());
+        return SDL_APP_FAILURE;
+    }
+
+    SDL_SetRenderVSync(renderer, 1);
+
+    // --- Print SDL versions ---
+    SDL_Log("Compiled SDL version: %d.%d.%d",
+        SDL_MAJOR_VERSION,
+        SDL_MINOR_VERSION,
+        SDL_MICRO_VERSION);
+    // Get the version of the SDL library linked at runtime
+    int v = SDL_GetVersion();
+    SDL_Log("Linked SDL3 version:  %d.%d.%d", SDL_VERSIONNUM_MAJOR(v),
+        SDL_VERSIONNUM_MINOR(v), SDL_VERSIONNUM_MICRO(v));
+
+    // --- SDL3_mixer version ---
+    v = MIX_Version();
+    int major = SDL_VERSIONNUM_MAJOR(v);
+    int minor = SDL_VERSIONNUM_MINOR(v);
+    int micro = SDL_VERSIONNUM_MICRO(v);
+    SDL_Log("SDL3_mixer version:   %d.%d.%d", major, minor, micro);
+
+    // --- Init mixer ---
+    if (!MIX_Init())
+    {
+        SDL_Log("MIX_Init failed: %s", SDL_GetError());
+        return SDL_APP_FAILURE;
+    }
+
+    mixer = MIX_CreateMixerDevice(SDL_AUDIO_DEVICE_DEFAULT_PLAYBACK, NULL);
+    if (!mixer)
+    {
+        SDL_Log("Couldn't create mixer: %s", SDL_GetError());
+        return SDL_APP_FAILURE;
+    }
+
+    // --- Print mixer spec ---
+    SDL_AudioSpec mspec;
+    MIX_GetMixerFormat(mixer, &mspec);
+    SDL_Log("Mixer format: %s, %d channels, %d Hz",
+        SDL_GetAudioFormatName(mspec.format),
+        mspec.channels,
+        mspec.freq);
+
+    // --- Print available decoders ---
+    SDL_Log("Available MIXER decoders:");
+
+    int ndec = MIX_GetNumAudioDecoders();
+    if (ndec < 0)
+    {
+        SDL_Log(" - [error: %s]", SDL_GetError());
+    }
+    else if (ndec == 0)
+    {
+        SDL_Log(" - [none]");
+    }
+    else
+    {
+        for (int i = 0; i < ndec; i++)
+        {
+            SDL_Log(" - %s", MIX_GetAudioDecoder(i));
+        }
+    }
+
+    // --- Load WAV audio file ---
+    const char *audiofile = "./assets/audio/picked-coin-echo-2.wav";
+    audio = MIX_LoadAudio(mixer, audiofile, false);
+
+    if (!audio)
+    {
+        SDL_Log("Failed to load '%s': %s", audiofile, SDL_GetError());
+    }
+    else
+    {
+        SDL_AudioSpec aspec;
+        MIX_GetAudioFormat(audio, &aspec);
+
+        SDL_Log("%s: %s, %d channel%s, %d Hz",
+            audiofile,
+            SDL_GetAudioFormatName(aspec.format),
+            aspec.channels,
+            (aspec.channels == 1 ? "" : "s"),
+            aspec.freq);
+    }
+
+    return SDL_APP_CONTINUE; // continue running
+}
+
+SDL_AppResult SDL_AppEvent(void *appstate, SDL_Event *event)
+{
+    if (event->type == SDL_EVENT_QUIT)
+    {
+        return SDL_APP_SUCCESS;
+    }
+    else if (event->type == SDL_EVENT_MOUSE_BUTTON_DOWN)
+    {
+        if (!MIX_PlayAudio(mixer, audio))
+        {
+            SDL_Log("Failed to play audio: %s", SDL_GetError());
+        }
+    }
+
+    return SDL_APP_CONTINUE;
+}
+
+SDL_AppResult SDL_AppIterate(void *appstate)
+{
+    SDL_SetRenderDrawColor(renderer, 210, 220, 210, SDL_ALPHA_OPAQUE);
+    SDL_RenderClear(renderer);
+    SDL_RenderPresent(renderer);
+    return SDL_APP_CONTINUE;
+}
+
+void SDL_AppQuit(void *appstate, SDL_AppResult result)
+{
+    if (audio)
+    {
+        MIX_DestroyAudio(audio);
+        audio = NULL;
+    }
+
+    if (mixer)
+    {
+        MIX_DestroyMixer(mixer);
+        mixer = NULL;
+    }
+
+    if (renderer)
+    {
+        SDL_DestroyRenderer(renderer);
+        renderer = NULL;
+    }
+
+    if (window)
+    {
+        SDL_DestroyWindow(window);
+        window = NULL;
+    }
+
+    MIX_Quit();
+    SDL_Quit();
+}
